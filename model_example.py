ffda3d9d16e871ae2b34426a46c2d85afda61c56
Bing-Yue Wu <bywu@asu.edu>
2023-12-19 18:49:45 -0800

    add ML example loss computation

--- model_example.py
+++ model_example.py
@@ -111,7 +111,6 @@ if __name__ == "__main__":
   #get golden map#
   ################
   row_height, track_width, static_IR_map = OpenROAD_map_creation("static_IR", tech_design, design, corner, -1)        
-  print(static_IR_map.shape)
 
   #handle size by padding or cropping
   static_power_map = handle_size(static_power_map, (256, 256))
@@ -119,17 +118,22 @@ if __name__ == "__main__":
   m1_congestion_map = handle_size(m1_congestion_map, (256, 256))
   m2_congestion_map = handle_size(m2_congestion_map, (256, 256))
   m3_congestion_map = handle_size(m3_congestion_map, (256, 256))
-  
+  static_IR_map = handle_size(static_IR_map, (256, 256))
+
   for i in range(len(m3_congestion_map)):
     static_power_map[i] = torch.Tensor(static_power_map[i]).unsqueeze(0).unsqueeze(0)
     dynamic_power_map[i] = torch.Tensor(dynamic_power_map[i]).unsqueeze(0).unsqueeze(0)
     m1_congestion_map[i] = torch.Tensor(m1_congestion_map[i]).unsqueeze(0).unsqueeze(0)
     m2_congestion_map[i] = torch.Tensor(m2_congestion_map[i]).unsqueeze(0).unsqueeze(0)
     m3_congestion_map[i] = torch.Tensor(m3_congestion_map[i]).unsqueeze(0).unsqueeze(0)
+    static_IR_map[i] = torch.Tensor(static_IR_map[i])
 
-  print(static_power_map[0].shape)
-
-  output_tensor = model([static_power_map[0], dynamic_power_map[0], m1_congestion_map[0], m2_congestion_map[0], m3_congestion_map[0]])
+  output_tensor = model([static_power_map[0], dynamic_power_map[0], m1_congestion_map[0], m2_congestion_map[0], m3_congestion_map[0]]).squeeze()
 
   output_array = output_tensor.squeeze().detach().numpy()
-
+  
+  l1loss = nn.L1Loss()
+  loss = l1loss(static_IR_map[0], output_tensor)
+  print(loss)
+  
+  
